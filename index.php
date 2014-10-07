93662fe3012d2d6083191cd812580e7a5356daf8
Al Nemec <al.nemec@gmail.com>
2014-10-07 10:18:44 -0500

    WP Clarity Admin UI

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+Plugin Name: Clarity Admin UI
+Plugin URI: http://alnemec.com/modern-admin-ui/
+Description: Clarity WordPress Admin Theme.
+Author: Al Nemec
+Version: 1.0
+Author URI: http://alnemec.com
+*/
+
+function my_admin_theme_style() {
+    wp_enqueue_style('my-admin-theme', plugins_url('wp-admin-master.css', __FILE__));
+    
+    wp_enqueue_script(
+		'my-admin-theme-actions',
+		plugins_url( 'js/wp-clean-admin-master.min.js' , __FILE__ ),
+		array( 'jquery' )
+	);
+}
+add_action('admin_enqueue_scripts', 'my_admin_theme_style');
+add_action('login_enqueue_scripts', 'my_admin_theme_style');
+
+
+function my_login_css() {
+  echo '<link rel="stylesheet" type="text/css" href="' .plugins_url('login.css  ', __FILE__). '">';
+}
+
+add_action('login_head', 'my_login_css');
+
+
+
+?>
